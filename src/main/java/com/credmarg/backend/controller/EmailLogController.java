1aa1a09a147c4b0a9c3b3c2d399f3cdb64cf2a0a
Your Name <irfankh6102@gmail.com>
2024-07-14 22:52:03 +0530

    Adding CrossOrigin in EmailLogController

--- src/main/java/com/credmarg/backend/controller/EmailLogController.java
+++ src/main/java/com/credmarg/backend/controller/EmailLogController.java
@@ -3,6 +3,7 @@ package com.credmarg.backend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.credmarg.backend.model.EmailLog;
 import com.credmarg.backend.service.EmailLogService;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/admin/api/emailLogs")
 public class EmailLogController {
